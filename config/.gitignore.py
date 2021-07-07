db241863e120b688f99aeaaaa1a848e1c2dec9e3
= <=>
2021-07-07 20:40:27 +0300

    Docler files cofigured

---
+++ config/.gitignore.py
@@ -0,0 +1,13 @@
+# Default ignored files
+/shelf/
+/workspace.xml
+# Datasource local storage ignored files
+/dataSources/
+/dataSources.local.xml
+# Editor-based HTTP Client requests
+/httpRequests/
+# IDE
+.idea/
+# Byte-compiled / optimized / DLL files
+_pychare_/
+*.py[cod]
